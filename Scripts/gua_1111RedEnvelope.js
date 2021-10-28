105caae3f696d53816fc54c2705eacba34ba6042
FengG <windfgg@gmail.com>
2021-10-28 19:14:27 +0800

    修改

--- Scripts/gua_1111RedEnvelope.js
+++ Scripts/gua_1111RedEnvelope.js
@@ -146,7 +146,7 @@ function getCoupons(shareId = "") {
     let opts = {
       url: `https://api.m.jd.com/api?functionId=getCoupons&appid=u&_=${Date.now()}&loginType=2&body={%22platform%22:4,%22unionActId%22:%2231134%22,%22actId%22:%22${
         $.actId
-      }%22,%22d%22:%223C7eCOr%22,%22unionShareId%22:%22${shareId}%22,%22type%22:1,%22eid%22:%22${
+      }%22,%22d%22:%223dVdwMF%22,%22unionShareId%22:%22${shareId}%22,%22type%22:1,%22eid%22:%22${
         $.eid
       }%22}&client=apple&clientVersion=8.3.6&h5st=undefined`,
       headers: {
@@ -230,7 +230,7 @@ function shareUnionCoupon() {
         $.actId
       }%22,%22platform%22:4,%22unionShareId%22:%22${
         $.shareCode
-      }%22,%22d%22:%223C7eCOr%22,%22supportPic%22:2,%22supportLuckyCode%22:0,%22eid%22:%22${
+      }%22,%22d%22:%223dVdwMF%22,%22supportPic%22:2,%22supportLuckyCode%22:0,%22eid%22:%22${
         $.eid
       }%22}&client=apple&clientVersion=8.3.6`,
       headers: {
@@ -251,8 +251,8 @@ function shareUnionCoupon() {
           if (typeof res == "object") {
             if (res.code == 0 && res.data && res.data.shareUrl) {
               $.shareCode =
-                (res.data.shareUrl.match(/3C7eCOr\?s=([^&]+)/) &&
-                  res.data.shareUrl.match(/3C7eCOr\?s=([^&]+)/)[1]) ||
+                (res.data.shareUrl.match(/3dVdwMF\?s=([^&]+)/) &&
+                  res.data.shareUrl.match(/3dVdwMF\?s=([^&]+)/)[1]) ||
                 "";
               console.log("分享码:" + $.shareCode);
               if ($.shareCode)
